bb72ac75a11da1728ff25480d9ce1a1e27fc92d8
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-02-28 20:52:41 +0500

    Implementing INotifyPropertyChanged

--- WinForms.Mvvm/ViewModels/PersonViewModel.vb
+++ WinForms.Mvvm/ViewModels/PersonViewModel.vb
@@ -1,8 +1,11 @@
-﻿Public Class PersonViewModel
+﻿Imports System.ComponentModel
+
+Public Class PersonViewModel
+    Implements INotifyPropertyChanged
     Public Overridable Property NationalIdCardNumber As String
     Public Overridable Property FirstName As String
     Public Overridable Property Lastname As String
     Public Overridable Property Address As Address
     Public Overridable Property ContactDetails As Contact
-
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
 End Class
