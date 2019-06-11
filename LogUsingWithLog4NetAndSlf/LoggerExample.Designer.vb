3066d78697a0f0a921347426fdecd17b8ec619de
Ibrahim Hussain <ibrahim.hucyn@live.com>
2019-06-11 21:32:50 +0500

    IMPLEMENTED LOGGING USING SLF4NET AND LOG4NET

---
+++ LogUsingWithLog4NetAndSlf/LoggerExample.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LoggerExample
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonThrowEx = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ButtonThrowEx
+        '
+        Me.ButtonThrowEx.Location = New System.Drawing.Point(117, 33)
+        Me.ButtonThrowEx.Name = "ButtonThrowEx"
+        Me.ButtonThrowEx.Size = New System.Drawing.Size(104, 23)
+        Me.ButtonThrowEx.TabIndex = 0
+        Me.ButtonThrowEx.Text = "Throw Ex"
+        Me.ButtonThrowEx.UseVisualStyleBackColor = True
+        '
+        'LoggerExample
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 83)
+        Me.Controls.Add(Me.ButtonThrowEx)
+        Me.Name = "LoggerExample"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ButtonThrowEx As Button
+End Class
---
+++ LogUsingWithLog4NetAndSlf/LoggerExample.vb
@@ -0,0 +1,24 @@
+﻿
+Imports slf4net
+
+<Assembly: log4net.Config.XmlConfigurator(Watch:=True)>
+Public Class LoggerExample
+
+    Private Shared ReadOnly _logger As ILogger = LoggerFactory.GetLogger(GetType(LoggerExample))
+
+    Public Sub Foo()
+        _logger.Trace("Foo started at {0}.", Date.Now)
+
+        ' Do some work
+
+        _logger.Trace("Foo() completed at {0}.", Date.Now)
+    End Sub
+
+    Private Sub ButtonThrowEx_Click(sender As Object, e As EventArgs) Handles ButtonThrowEx.Click
+        Try
+            Throw New NotImplementedException
+        Catch ex As Exception
+            _logger.Error(ex.Message)
+        End Try
+    End Sub
+End Class
