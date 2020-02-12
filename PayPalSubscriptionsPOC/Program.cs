107c0bfa8153f7b83f0b1fcb1bf098ea4bee9f6a
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-02-13 01:57:07 +0500

    Deserialised the access token response into a class

--- PayPalSubscriptionsPOC/Program.cs
+++ PayPalSubscriptionsPOC/Program.cs
@@ -1,4 +1,5 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json;
+using RestSharp;
 using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
@@ -12,31 +13,60 @@ namespace PayPalSubscriptionsPOC
     {
         static void Main(string[] args)
         {
-            string clientId = "AVeLPjw99V_seFhy9ByHAhgZcr2UA0TZRrWiU0yZXBhgZbqIDQjiz3reFqTmH9ozFa61HE8cvlMwvCtG";
-            string clientSecret = "EDaCNfV0c8R19xKYqjAqEuAOeZopeBP7OqKuZ1NbbSUOF9C-IeNAHZqbVPWNv-jIU4KOStjsF41kvZbp";
-
-            var client = new RestClient("https://api.sandbox.paypal.com/v1/oauth2/token");
-
-            client.Authenticator = new HttpBasicAuthenticator(clientId, clientSecret);
-            client.Timeout = -1;
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-            request.AddParameter("grant_type", "client_credentials");
-            IRestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
-
-
+            var a = PayPalAuth.GetAccessToken(new PaypalClient());
             Console.ReadLine();
 
+        }
+
+        public class PayPalAuth
+        {
+            public static AccessToken GetAccessToken(PaypalClient AuthData)
+            {
+                var client = new RestClient(AuthData.sandboxUrl)
+                {
+                    Authenticator = new HttpBasicAuthenticator(AuthData.clientId, AuthData.clientSecret),
+                    Timeout = -1
+                };
 
 
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                request.AddParameter("grant_type", "client_credentials");
+
+                IRestResponse response = client.Execute(request);
+                return JsonConvert.DeserializeObject<AccessToken>(response.Content);
+            }
         }
 
         public class AccessToken
         {
+            private string _scope { get; set; }
+            public string scope 
+            {
+                get
+                {
+                    return _scope;
+                }
+                set 
+                {
+                    this._scope = value;
+                    ScopeList = _scope.Split(' ').ToList();
+                }
+            }
+            public List<string>  ScopeList { get; private set; }
             public string access_token { get; set; }
             public string token_type { get; set; }
+            public string app_id { get; set; }
             public long expires_in { get; set; }
+            public string nonce { get; set; }
+        }
+
+        public class PaypalClient
+        {
+            public string clientId = "AVeLPjw99V_seFhy9ByHAhgZcr2UA0TZRrWiU0yZXBhgZbqIDQjiz3reFqTmH9ozFa61HE8cvlMwvCtG";
+            public string clientSecret = "EDaCNfV0c8R19xKYqjAqEuAOeZopeBP7OqKuZ1NbbSUOF9C-IeNAHZqbVPWNv-jIU4KOStjsF41kvZbp";
+            public string sandboxUrl = @"https://api.sandbox.paypal.com/v1/oauth2/token";
+
         }
     }
 }
