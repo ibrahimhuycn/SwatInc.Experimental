5bc6b6ea04c262966d666efc10fb55c0fc430f0d
Ibrahim Hussain <ibrahim.hucyn@live.com>
2020-02-18 12:50:01 +0500

    Able to bind the Value member by binding to SelectedValue

---
+++ SyncFusionComboDatabind/TestForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SyncFusionComboDatabind
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfComboBox1 = new Syncfusion.WinForms.ListView.SfComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sfComboBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sfComboBox1
+            // 
+            this.sfComboBox1.Location = new System.Drawing.Point(86, 45);
+            this.sfComboBox1.Name = "sfComboBox1";
+            this.sfComboBox1.Size = new System.Drawing.Size(239, 28);
+            this.sfComboBox1.Style.TokenStyle.CloseButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.sfComboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(249, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 168);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sfComboBox1);
+            this.Name = "TestForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sfComboBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.ListView.SfComboBox sfComboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SyncFusionComboDatabind/TestForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncFusionComboDatabind
+{
+    public partial class TestForm : Form
+    {
+        private TestFormViewModel _viewModel;
+
+        public TestForm(TestFormViewModel testFormViewModel)
+        {
+            InitializeComponent();
+            this._viewModel = testFormViewModel;
+            InitializeBinding();
+        }
+
+        private void InitializeBinding()
+        {
+            this.sfComboBox1.DataSource = _viewModel.person;
+            this.sfComboBox1.ValueMember = nameof(Person.Id);
+            this.sfComboBox1.DisplayMember = nameof(Person.FullName);
+
+            this.sfComboBox1.DataBindings.Add(new Binding("SelectedValue", _viewModel, nameof(_viewModel.SelectedName)));
+        }
+    }
+
+    public class TestFormViewModel : INotifyPropertyChanged
+    {
+        public TestFormViewModel()
+        {
+            this.person = new BindingList<Person>();
+            SetUpDemoData();
+        }
+
+        private void SetUpDemoData()
+        {
+            this.person.Add(new Person() { Id = 100, FullName = "Ahmed Hussain" });
+            this.person.Add(new Person() { Id = 200, FullName = "Ibrahim Hussain" });
+            this.person.Add(new Person() { Id = 300, FullName = "Khalid Hussain" });
+        }
+
+        public BindingList<Person> person;
+        private string selectedName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string SelectedName
+        {
+            get => selectedName;
+            set
+            {
+                selectedName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedName)));
+                Debug.WriteLine(value);
+            }
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+}
+
+
